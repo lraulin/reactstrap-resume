47623c99c60303113981f2f38d9c72b6d687a97d
Lee Raulin <leeraulin@gmail.com>
2019-08-22 10:00:27 -0400

    Updates

--- precache-manifest.cc2d708582288e1cf83bee5c65f2dd2d.js
+++ precache-manifest.cc2d708582288e1cf83bee5c65f2dd2d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6bacd3825f3792d72a4860d0c9613dcc",
+    "revision": "8c64e36a15e279b4bc2a69a19ef4da15",
     "url": "/reactstrap-resume/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactstrap-resume/static/css/2.266e55a5.chunk.css"
   },
   {
-    "revision": "30038f7cb46fcbf40a59",
+    "revision": "aa4e3f56949a76283635",
     "url": "/reactstrap-resume/static/css/main.34de6062.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/reactstrap-resume/static/js/2.6e35afcb.chunk.js"
   },
   {
-    "revision": "30038f7cb46fcbf40a59",
-    "url": "/reactstrap-resume/static/js/main.9c9c18a7.chunk.js"
+    "revision": "aa4e3f56949a76283635",
+    "url": "/reactstrap-resume/static/js/main.ddf5e5da.chunk.js"
   },
   {
     "revision": "48670435f08bac5d125f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/reactstrap-resume/precache-manifest.1af8a62d22f1e43780f06394124bc132.js"
+  "/reactstrap-resume/precache-manifest.cc2d708582288e1cf83bee5c65f2dd2d.js"
 );
 
 self.addEventListener('message', (event) => {
