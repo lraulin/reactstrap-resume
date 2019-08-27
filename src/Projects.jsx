d66830302910d6c0e0e618ecf3d342dcd52fcf5d
Lee Raulin <leeraulin@gmail.com>
2019-08-27 19:56:37 -0400

    Projects coming soon remove picture

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -3,12 +3,8 @@ import React from "react";
 const Projects = props => {
   return (
     <div>
-      <h1>Projects</h1>
+      <h1>Projects - Coming Soon!</h1>
       <p>{props.location.pathname}</p>
-      <img
-        src="http://southvault.com/wp-content/uploads/2018/09/Construction-Site-Security.jpg"
-        alt="construction"
-      />
     </div>
   );
 };
