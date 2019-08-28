e8319ad3a8a35bd5ec8609504a265c7df21296a5
Lee Raulin <leeraulin@gmail.com>
2019-08-28 12:11:13 -0400

    Format resume

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -40,6 +40,14 @@ const centered = {
   margin: "auto",
 };
 
+const floatLeft = {
+  float: "left",
+};
+
+const clear = {
+  clear: "both",
+};
+
 const popup = () => alert("Not yet implemented!");
 
 const Resume = props => {
@@ -73,28 +81,26 @@ const Resume = props => {
             <article id="resume">
               <header>
                 <h1>Lee M. Raulin</h1>
-                <p className="subtitle" style={{ centered }}>
-                  JavaScript/Python Developer
-                </p>
+                <p className="subtitle">JavaScript/Python Developer</p>
               </header>
               <section id="contactSection">
-                <span className="contactInfo phoneNumber">
+                <span className="contactInfo phoneNumber" style={floatLeft}>
                   <i className="fas fa-mobile-alt"></i> (443) 684-8939
                 </span>
-                <span className="contactInfo emailAddress">
+                <span className="contactInfo emailAddress" style={floatLeft}>
                   <a href="mailto:leeraulin@gmail.com">
                     <i className="fas fa-envelope-square"></i>{" "}
                     leeraulin@gmail.com
                   </a>
                 </span>
-                <span className="contactInfo streetAddress">
+                <span className="contactInfo streetAddress" style={floatLeft}>
                   <a href="https://www.google.com/maps/place/Chesapeake+Beach">
                     <i className="fas fa-map-marked-alt"></i> Chesapeake Beach,
                     MD
                   </a>
                 </span>
               </section>
-              <section id="summarySection">
+              <section id="summarySection" style={clear}>
                 <h2>Summary</h2>
                 <p>
                   Dedicated and disciplined self-taught programmer 8+ years of
@@ -176,9 +182,7 @@ const Resume = props => {
                 <ul>
                   <li>
                     <p>
-                      <a href="/projects/incident-map">
-                        Twitter Incident Report Map
-                      </a>{" "}
+                      <em>Twitter Incident Report Map: </em>
                       Reverse-engineered and reconstructed an app (with access
                       only to the front-end code) using JavaScript (ES6), React,
                       Redux, AWS (E2, RDS, S5), PostgreSQL, Mapbox, etc.
@@ -192,18 +196,15 @@ const Resume = props => {
                 <ul>
                   <li>
                     <p>
-                      <a href="/projects/dns-script">DNS Script</a> Coded a
-                      NodeJS/SQLite console app that automated checking hundreds
-                      of DNS servers every hour and emailing a daily summary of
-                      any discrepancies. Deployed to a government BIND (Linux)
-                      server.
+                      <em>DNS Script: </em>Constructed a NodeJS/SQLite console
+                      app that automated checking hundreds of DNS servers every
+                      hour and emailing a daily summary of any discrepancies.
+                      Deployed to a government BIND (Linux) server.
                     </p>
                   </li>
                   <li>
                     <p>
-                      <a href="/projects/vulnerability-visualization">
-                        Vulnerability Visualization
-                      </a>{" "}
+                      <em>Vulnerability Visualization: </em>
                       Programmed a Python script that triggers on receipt of an
                       email by a MS 365 account containing a report on security
                       vulnerabilities. It extracts files attached to the PDF
@@ -214,7 +215,7 @@ const Resume = props => {
                   </li>
                   <li>
                     <p>
-                      <a href="/projects/syslog_analytics">Syslog Analytics</a>{" "}
+                      <em>Syslog Analytics: </em>
                       Composed Python script to parse/reformat spreadsheet files
                       to support cyber team.
                     </p>
@@ -269,6 +270,7 @@ const Resume = props => {
                   city="St. Mary's City, MD"
                   schoolUrl="https://www.smcm.edu"
                   bulletPoints={[]}
+                  date="2004-06"
                 />
               </section>
             </article>
--- src/School.jsx
+++ src/School.jsx
@@ -4,6 +4,7 @@ const inlineBlock = { display: "inline-block" };
 const justifySpaceBetween = { justifyContent: "space-between" };
 
 const months = [
+  "",
   "January",
   "February",
   "March",
@@ -20,12 +21,17 @@ const months = [
 
 const formatDate = (date = "") => {
   if (date) {
-    date = new Date(date);
+    if (typeof date === "number") {
+      date = date.toString();
+    }
+    date = new Date(...date.split("-"));
     return `${months[date.getMonth()].slice(0, 3)} ${date.getFullYear()}`;
   } else {
     return "";
   }
 };
+
+console.log(formatDate(2004));
 
 const dateRange = (start, stop) => {
   [start, stop] = [start, stop].map(x => formatDate(x));
@@ -54,28 +60,38 @@ const School = ({
   console.log(dateText);
   return (
     <div>
-      {/* if (there is a degree url): make the header a link */}
-      {degreeUrl && (
-        <h3>
-          <a href={schoolUrl}>{degree}</a>
-        </h3>
-      )}
-      {/* else: no link, just header */}
-      {!degreeUrl && <h3>{degree}</h3>}
+      <div className="line1">
+        {/* if (there is a degree url): make the header a link */}
+        {degreeUrl && (
+          <h3 style={{ textAlign: "left", float: "left" }}>
+            <a href={schoolUrl}>{degree}</a>
+          </h3>
+        )}
+        {/* else: no link, just header */}
+        {!degreeUrl && (
+          <h3 style={{ textAlign: "left", float: "left" }}>{degree}</h3>
+        )}
 
-      {/* date(s) if any */}
-      {(date || startDate) && <p style={{ float: "right" }}>{dateText}</p>}
+        {/* date(s) if any */}
+        {(date || startDate) && (
+          <p style={{ textAlign: "right", float: "right" }}>{dateText}</p>
+        )}
+      </div>
+      <div className="line2" style={{ clear: "both" }}>
+        {/* SCHOOL NAME */}
+        <a style={{ textAlign: "left", float: "left" }} href={schoolUrl}>
+          {schoolName}
+        </a>
 
-      <a href={schoolUrl}>{schoolName}</a>
-
-      {/* city, if exists */}
-      {city && (
-        <p className="city" style={{ float: "right" }}>
-          {" "}
-          {city}
-        </p>
-      )}
-      <ul>
+        {/* city, if exists */}
+        {city && (
+          <p className="city" style={{ textAlign: "right", float: "right" }}>
+            {" "}
+            {city}
+          </p>
+        )}
+      </div>
+      <ul style={{ clear: "both" }}>
         {bulletPoints.map(point => (
           <li key={point}>{point}</li>
         ))}
