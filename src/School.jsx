2bdcbaf05551603671cda43d5b99aa38b7ab56a3
Lee Raulin <leeraulin@gmail.com>
2019-08-28 10:19:32 -0400

    Improve Education section

--- src/School.jsx
+++ src/School.jsx
@@ -3,23 +3,78 @@ import React from "react";
 const inlineBlock = { display: "inline-block" };
 const justifySpaceBetween = { justifyContent: "space-between" };
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const formatDate = (date = "") => {
+  if (date) {
+    date = new Date(date);
+    return `${months[date.getMonth()].slice(0, 3)} ${date.getFullYear()}`;
+  } else {
+    return "";
+  }
+};
+
+const dateRange = (start, stop) => {
+  [start, stop] = [start, stop].map(x => formatDate(x));
+  if (start && stop) {
+    return `${start} ~ ${stop}`;
+  } else if (start) {
+    return start;
+  } else if (stop) {
+    return stop;
+  } else {
+    return null;
+  }
+};
+
 const School = ({
-  schoolUrl = "",
   schoolName = "",
+  schoolUrl = "",
   degree = "",
+  degreeUrl = "",
   city = "",
   bulletPoints = [],
+  startDate = "",
+  date = "",
 }) => {
-  console.log(degree);
+  const dateText = dateRange(startDate, date);
+  console.log(dateText);
   return (
     <div>
-      <h3>{degree}</h3>
-      <div style={justifySpaceBetween}>
-        <a style={inlineBlock} href={schoolUrl}>
-          {schoolName}
-        </a>
-        {city && <p style={inlineBlock}> {" | " + city}</p>}
-      </div>
+      {/* if (there is a degree url): make the header a link */}
+      {degreeUrl && (
+        <h3>
+          <a href={schoolUrl}>{degree}</a>
+        </h3>
+      )}
+      {/* else: no link, just header */}
+      {!degreeUrl && <h3>{degree}</h3>}
+
+      {/* date(s) if any */}
+      {(date || startDate) && <p style={{ float: "right" }}>{dateText}</p>}
+
+      <a href={schoolUrl}>{schoolName}</a>
+
+      {/* city, if exists */}
+      {city && (
+        <p className="city" style={{ float: "right" }}>
+          {" "}
+          {city}
+        </p>
+      )}
       <ul>
         {bulletPoints.map(point => (
           <li key={point}>{point}</li>
